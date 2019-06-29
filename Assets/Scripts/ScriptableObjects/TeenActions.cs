15e3217dfbacbc0aee058326a0d7eee20494720a
Julian Hernandez <gmfk07@gmail.com>
2019-06-29 14:43:04 -0400

    Health and Sanity

--- Assets/Scripts/ScriptableObjects/TeenActions.cs
+++ Assets/Scripts/ScriptableObjects/TeenActions.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/TeenAction", order = 1)]
-public class TeenActions : ScriptableObject
+public class TeenAction : ScriptableObject
 {
-    [Tooltip("The chat message that displays, the substring <> will be replaced with teen's name.")]public string message;
-    public int damage;
-    public int insight;
-    public int healHealth;
-    public int healSanity;
+    [Tooltip("The chat message that displays, the substring <> will be replaced with teen's name.")] public string message;
+    public string Name;
+    public int Damage;
+    public int Insight;
+    public int HealHealth;
+    public int HealSanity;
 }
--- Assets/Scripts/Teen.cs
+++ Assets/Scripts/Teen.cs
@@ -9,17 +9,48 @@ public class Teen : MonoBehaviour, IPointerClickHandler
     public string Name;
     [SerializeField] private int maxHealth;
     [SerializeField] private int maxSanity;
+    [SerializeField] private MessageManager messageManager;
     [SerializeField] private ActionPanel actionPanel;
     [SerializeField] private GameObject button;
-    [SerializeField] private List<string> buttons;
+    [SerializeField] private Text healthText;
+    [SerializeField] private Text sanityText;
+    [SerializeField] private List<TeenAction> availableActions;
+
+    private int health;
+    private int sanity;
+
+    private void Start()
+    {
+        health = maxHealth;
+        sanity = maxSanity;
+    }
 
     public void OnPointerClick(PointerEventData pointerEventData)
     {
         actionPanel.Display();
-        for (int i = 0; i < buttons.Count; i++)
+        for (int i = 0; i < availableActions.Count; i++)
         {
             GameObject newButton = Instantiate(button, actionPanel.transform);
-            newButton.GetComponentInChildren<Text>().text = buttons[i];
+            newButton.GetComponentInChildren<Text>().text = availableActions[i].message.Replace("<>", Name);
+            newButton.GetComponent<Button>().onClick.AddListener(() => PerformTeenAction(availableActions[i]));
         }
+    }
+
+    private void PerformTeenAction(TeenAction action)
+    {
+        ChangeHealth(action.HealSanity);
+        ChangeSanity(action.HealHealth);
+    }
+
+    private void ChangeHealth(int delta)
+    {
+        health = Mathf.Min(health + delta, maxHealth);
+        healthText.text = health.ToString() + "/" + maxHealth.ToString();
+    }
+
+    private void ChangeSanity(int delta)
+    {
+        sanity = Mathf.Min(sanity + delta, maxSanity);
+        sanityText.text = sanity.ToString() + "/" + maxSanity.ToString();
     }
 }
